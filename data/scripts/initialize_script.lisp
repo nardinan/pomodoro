05dd4014b8638aee3a7dab957a18072e790264d3
nardinan <andrea@nardinan.it>
2016-11-09 02:45:00 +0100

    Some items
    Some items have been imported in a test scenario (Canama House) in order to check the stability of the code.

--- data/scripts/initialize_script.lisp
+++ data/scripts/initialize_script.lisp
@@ -17,27 +17,32 @@
 	)
 )
 
+(define main_control
+	(lambda (c)
+		(begin
+			(director_camera_follow c -100.0 1.0)
+			(puppeteer_enable_control c)
+		)
+	)
+)
+
 (puppeteer_show "andrea" 200)
 (puppeteer_show "andrii" 800)
 (puppeteer_disable_control)
-(director_camera_follow "andrea" -100.0 1.0)
 (say "andrea" "Hello guys!")
 (say "andrii" "Hello!")
 (animation "andrea" "still_right")
 (say "andrea" "What do you think about my script system, Andrii?")
 (animation "andrii" "still_left")
 (effecteer_add "expl1" "smoke_spot_particle" 400 450)
-(director_wait 2)
+(director_wait 1)
 (effecteer_add "expl2" "fire_spot_particle" 600 450 1 0 nil t)
-(director_wait 2)
-(say "andrii" ". . .")
 (say "andrii" "Well . . . it works at least!")
-(say "andrea" "You hate it!")
-(say "andrii" "No, you hate it!")
 (animation "andrii" "back_code")
-(puppeteer_enable_control "andrea")
 
-(director_wait 10)
+(main_control "andrea")
+
+(director_wait 1)
 
 (effecteer_stop "expl1")
 (effecteer_stop "expl2")
