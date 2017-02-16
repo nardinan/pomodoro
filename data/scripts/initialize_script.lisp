d8c95613cf4b1d6bc45764b712d2f3c7ff7ceb11
nardinan <andrea@nardinan.it>
2017-02-16 19:44:27 +0100

    Initialize script has been modified
    Initialize script has been modified in order to re-start from the last visited scenario

--- data/scripts/initialize_script.lisp
+++ data/scripts/initialize_script.lisp
@@ -6,6 +6,11 @@
 ; Preamble. Here we should check for other things: loaded informations and stored informations.
 (director_camera_set 1000.0 -1000.0) ; far far away ...
 (if (collector_get "intro_ran")
-	(stagecrafter_show "canama")
+    (begin
+        (if (collector_get "destination_scenario")
+          (stagecrafter_show (collector_get "destination_scenario"))
+          (stagecrafter_show "canama")
+        )
+    )
 	(stagecrafter_show "cern")
 )
