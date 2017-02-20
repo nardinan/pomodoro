bce29413025cc7ee9e904108009ff5003f10ecb0
nardinan <andrea@nardinan.it>
2017-02-21 00:02:12 +0100

    Bubble issue has been fixed

--- character.obj.c
+++ character.obj.c
@@ -331,8 +331,8 @@ d_define_method_override(character, draw)(struct s_object *self, struct s_object
         total_zoom = drawable_attributes_self->zoom * environment_attributes->zoom[environment_attributes->current_surface];
         bubble_position_x = position_x + (character_attributes->bubble_offset_x * total_zoom);
         bubble_position_y = position_y + (character_attributes->bubble_offset_y * total_zoom) - bubble_attributes->total_height;
-        if (((bubble_position_x * ratio_x) + bubble_attributes->maximum_width - environment_attributes->camera_origin_x[environment_attributes->current_surface]) > 
-                environment_attributes->current_w) {
+        if (((bubble_position_x * ratio_x) + bubble_attributes->maximum_width * ratio_x - 
+                    environment_attributes->camera_origin_x[environment_attributes->current_surface]) > environment_attributes->current_w) {
             bubble_attributes->current_position = e_bubble_position_left;
             bubble_position_x -= bubble_attributes->maximum_width;
         } else
