8b12815c294cf3fdd724ad376e8a140b92c8c9e1
nardinan <andrea@nardinan.it>
2017-04-11 14:50:05 +0200

    CERN Intro
    An example of dialogs with audio tracks (CERN intro) has been included in the game but is still not sure that we will follow this road

--- data/scripts/intro_cern.lisp
+++ data/scripts/intro_cern.lisp
@@ -9,157 +9,166 @@
 (define dialogs (list 
 	(cons ;0 andrii
  	 "Dilution magnets are now operative and ready to prevent a catastophic scenario"
-	 "I magneti di diluizione sono operativi e pronti a prevenire una possibile catastrofe")
+	 "I magneti di diluizione stati attivati e sono pronti a prevenire una possibile catastrofe")
 	(cons ;1 yuriy
 	 "That's great"
 	 "Fantastico")
 	(cons ;2 yuriy
-	 "Dr. Tykhonov, could you please disable security grids?"
-	 "Dr. Tykhonov, puo' cortesemente disabilitare le griglie di sicurezza?")
-	(cons ;3 andrii
 	 "Dr. Tykhonov?"
 	 "Dr. Tykhonov?")
-	(cons ;4 andrii
-	 "Sounds too much formal, don't you think?"
-	 "Suona un po' troppo formale, non credi?")
-	(cons ;5 yuriy
-	 "In 'Angel & Deamons' people at CERN are referring each other in this way!"
-	 "Su 'Angeli & Demoni' le persone al CERN usano questi appellativi!")
+	(cons ;3 andrii
+	 "Mh?"
+	 "Mh?")
+	(cons ;4 yuriy
+	 "Could you please disable the security grid?"
+	 "Potrebbe cortesemente disabilitare la griglia di sicurezza?")
+	(cons ;5 andrii
+	 "Dr. Tykhonov?"
+	 "Dr. Tykhonov?")
 	(cons ;6 andrii
+	 "Sounds too much formal, don't you think?"
+	 "Suona leggermente troppo formale, non credi?")
+	(cons ;7 yuriy
+	 "In 'Angels & Daemons' CERN's people referring each other in this way"
+	 "Ma su 'Angeli & Demoni' le persone al CERN usano questi appellativi!")
+	(cons ;8 andrii
 	 "By the way ..."
 	 "Comunque ...")
-	(cons ;7 andrii
+	(cons ;9 andrii
 	 "I don't think we should disable the security grids, Yuriy"
-	 "Non credo sia saggio disabilitare le griglie di sicurezza, Yuriy")
-	(cons ;8 andrii
+	 "Non credo che dovremmo disabilitare le griglie di sicurezza, Yuriy")
+	(cons ;10 andrii
 	 "Is too risky"
 	 "E' troppo pericoloso")
-	(cons ;9 yuriy
+	(cons ;11 yuriy
 	 "Oh, please ..."
-	 "Oh, per favore ...")
-	(cons ;10 andrii
-	 "Fine"
-	 "Va bene")
-	(cons ;11 andrii
+	 "Oh, eddai!")
+	(cons ;12 andrii
+	 "Nope"
+	 "No")
+	(cons ;13 yuriy
+	 "Oh, please ..."
+	 "Oh, essu!")
+	(cons ;14 andrii
+	 "Fine, asshole"
+	 "E va bene, rompipalle!")
+	(cons ;15 andrii
 	 "Security grids have been disabled, Dr. Ilchenko"
 	 "Le griglie di sicurezza sono state disabilitate, Dr. Ilchenko")
-	(cons ;12 yuriy
+	(cons ;16 yuriy
 	 "Great"
-	 "Fantastico")
-	(cons ;13 yuriy
-	 "Dan Brown would be proud of you!"
-	 "Dan Brown sarebbe orgoglioso di te!")
-	(cons ;14 andrii
+	 "Fantastico! Dan Brown sarebbe orgoglioso di te!")
+	(cons ;17 andrii
 	 "Dan Brown is an needy"
 	 "Dan Brown e' un disagiato")
-	(cons ;15 yuriy
+	(cons ;18 yuriy
 	 "Uhm"
 	 "Uhm")
-	(cons ;16 yuriy
+	(cons ;19 yuriy
 	 "LHC is smoking. Is it normal?"
-	 "Del fumo esce dall'LHC. E' normale?")
-	(cons ;17 andrii
-	 "No Yuriy, obviously, is not normal."
+	 "Andrii, del fumo esce dall'LHC! E' normale?")
+	(cons ;20 andrii
+	 "No Yuriy, obviously, it is not normal."
 	 "No Yuriy, ovviamente non e' normale.")
-	(cons ;18 yuriy
+	(cons ;21 yuriy
 	 "Is called 'sarcasm'. Even Dan Brown would be able to recognize it"
-	 "Si chiama 'sarcasmo'. Anche Dan Brown sarebbe capace di riconoscerlo.")
-	(cons ;19 andrii
+	 "Si chiama 'sarcasmo' e anche Dan Brown sarebbe capace di riconoscerlo!")
+	(cons ;22 andrii
 	 "Probably we should shut it off right now before it gets worse"
-	 "Forse e' il caso di spegnerlo subito prima che la situazione peggiori")
-	(cons ;20 yuriy
+	 "Credo sia il caso di spegnerlo prima che la situazione peggiori")
+	(cons ;23 yuriy
 	 "Uh?"
 	 "Uh?")
-	(cons ;21 yuriy
+	(cons ;24 yuriy
 	 "Worse?"
 	 "Peggiorare?")
-	(cons ;22 yuriy
+	(cons ;25 yuriy
 	 "It's smoking! How could it be worse?"
-	 "Sta uscendo del fumo! Come puo' peggiore la situazione?")
-	(cons ;23 yuriy
+	 "Sta uscendo del fumo! Come puo' peggiorare la situazione?")
+	(cons ;26 yuriy
 	 "Oh, I see ..."
-	 "Oh, capisco ...")
-	(cons ;24 andrii
+	 "Oh, capisco ora!")
+	(cons ;27 andrii
 	 "Fuck"
-	 "Cazzo")
-	(cons ;25 andrii
-	 "Shit"
 	 "Merda")
-	(cons ;26 andrii
+	(cons ;28 andrii
+	 "Shit"
+	 "Puttana Eva")
+	(cons ;29 andrii
 	 "Diomatonne(TM)"
 	 "Diomatonne(TM)")
-	(cons ;27 andrii
+	(cons ;30 andrii
 	 "We are polluting the fabric of space-time"
-	 "Stiamo seriamente inquinando il tessuto spazio-termporale")
-	(cons ;28 yuriy
+	 "Stiamo inquinando il tessuto spazio-tempo")
+	(cons ;31 yuriy
 	 "What?"
 	 "Cosa?")
-	(cons ;29 yuriy
+	(cons ;32 yuriy
 	 "Fabric of space-time?"
-	 "Il tessuto spazio-temporale?")
-	(cons ;30 yuriy
+	 "Il tessuto spazio-tempo?")
+	(cons ;33 yuriy
 	 "We are polluting your brain as well . . ."
-	 "Stiamo inquinando anche il tuo cervello . . .")
-	(cons ;31 andrii
+	 "Ti sei laureato su Topolino?")
+	(cons ;34 andrii
 	 "Shut the fuck up!"
-	 "Stai zitto, cazzo!")
-	(cons ;32 andrii
+	 "Stai zitto!")
+	(cons ;35 andrii
 	 "Jesus Christ, shut the fucking crap up!"
 	 "Gesu' Cristo, smettila di parlare!")
-	(cons ;33 yuriy
-	 "Phew"
-	 "Phew")
-	(cons ;34 yuriy
-	 "You did it!"
-	 "Ce l'hai fatta!")
-	(cons ;35 andrii
-	 "Well, yes but ..."
-	 "Beh, si ma ...")
 	(cons ;36 yuriy
-	 "'But' what?"
-	 "'Ma' cosa?")
+	 "Phew"
+	 "Phew, per un pelo")
 	(cons ;37 andrii
-	 "The energy peak that we saw wasn't casual"
-	 "Il picco d'energia che abbiamo visto non era casuale")
+	 "Uhm"
+	 "Uhm")
 	(cons ;38 yuriy
-	 "So?"
-	 "Quindi?")
+	 ""
+	 "Cosa? Che c'e'?")
 	(cons ;39 andrii
-	 "I think we have changed something, somewhere in the past"
-	 "Credo che abbiamo cambiato qualcosa, da qualche parte nel passato")
-	(cons ;40 andrii
-	 "We have changed ..."
-	 "Abbiamo cambiato ...")
+	 ""
+	 "Yuriy io credo che il picco d'energia che abbiamo avuto non sia stato casuale")
+	(cons ;40 yuriy
+	 ""
+	 "Spiegazione, prego")
 	(cons ;41 andrii
-	 "THE HISTORY"
+	 ""
+	 "Vedi, secondo il computer abbiamo avuto un blackout dei magneti di controllo")
+	(cons ;42 andrii
+	 ""
+	 "Potremmo aver  cambiato qualcosa da qualche parte nel passato")
+	(cons ;43 andrii
+     ""
+     "Potremmo aver cambiato . . .")
+	(cons ;44 andrii
+	 ""
 	 "LA STORIA")
 	(cons ;42 yuriy
-	 "Fuck! I forgot my Umbrella"
-	 "Merda! Mi son dimenticato l'ombrello!")
+	 ""
+	 "Oh no, ho dimenticato l'ombrello a casa!")
 	(cons ;43 yuriy
-	 "Anyway we are still here!"
+	 ""
 	 "Comunque noi siamo ancora qui!")
 	(cons ;44 yuriy
-	 "Probably we've changed the history of some dumb person . . ."
+	 "" 
 	 "Probabilmente abbiamo cambiato la storia di qualche idiota . . .")
 	(cons ;45 andrii
-	 "Maybe we've changed the history of a group of dumb persons"
-	 "O magari di un gruppo di idioti")
+	 ""
+	 "O magari di un gruppo di idioti!")
 	(cons ;46 yurii
-	 "In a nameless city, maybe"
-	 "Magari in una citta' senza nome")
+	 ""
+	 "Magari in una citta' senza nome . . .")
 	(cons ;47 andrii
-	 "The kind of city that you couldn't find in a map"
-	 "La solita citta' che pur con una cartina davanti nessuno saprebbe dove cercarla")
+	 ""
+	 "La solita citta' che pur con una cartina davanti, nessuno saprebbe dove cercarla!")
 	(cons ;48 yuriy
-	 "Probably not many years ago"
-	 "Magari in un passato non troppo lontano")
+	 ""
+	 "Magari in un passato non troppo lontano . . .")
 	(cons ;49 andrii
-	 "Would we tell how it ends?"
-	 "Dobbiamo raccontare come va a finire?")
+	 ""
+	 "Che dobbiamo raccontare come va a finire?")
 	(cons ;50 yuriy
 	 "Right, it's enough"
-	 "Giuso, famo basta")
+	 "Hai ragione, famo basta")
 	(cons ;51 yuriy
 	 "Vodka party?"
 	 "Vodka party?")
@@ -178,10 +187,27 @@
 	)
 )
 
+(define talk
+	(lambda (c m track)
+		(begin
+			(puppeteer_talk c m track)
+			(director_wait_message c)
+		)
+	)
+)
+
 (define say_and_go
 	(lambda (c m)
 		(begin
 			(puppeteer_say c m (+ 1 (* (length m) 0.05)))
+		)
+	)
+)
+
+(define talk_and_go
+	(lambda (c m track)
+		(begin
+			(puppeteer_talk c m track)
 		)
 	)
 )
@@ -230,37 +256,40 @@
 (stagecrafter_set_item "computer_A" "coding_A")
 (stagecrafter_set_item "computer_C" "coding_C")
 (director_wait_time 2)
-(say "andrii"	(get_dialog dialogs language 0))
-(say "yuriy"	(get_dialog dialogs language 1))
+(talk "andrii"	(get_dialog dialogs language 0) "andrii_track1")
+(talk "yuriy"	(get_dialog dialogs language 1) "yuriy_track1")
 (director_wait_time 1)
 (animation "yuriy" 	"still_right")
 (stagecrafter_set_item "computer_A" "still_A")
-(say "yuriy"	(get_dialog dialogs language 2))
+(talk "yuriy"	(get_dialog dialogs language 2) "yuriy_track2")
+(talk "andrii"	(get_dialog dialogs language 3) "andrii_track2")
+(talk "yuriy"	(get_dialog dialogs language 4)	"yuriy_track3")
 (animation "andrii" "back")
 (stagecrafter_set_item "computer_C" "still_C")
 (director_wait_time 1)
 (animation "andrii" "still_left")
-(say "andrii"	(get_dialog dialogs language 3))
-(say "andrii"	(get_dialog dialogs language 4))
-(say "yuriy"  	(get_dialog dialogs language 5))
+(talk "andrii"	(get_dialog dialogs language 5) "andrii_track3")
+(talk "andrii"	(get_dialog dialogs language 6) "andrii_track4")
+(talk "yuriy"  	(get_dialog dialogs language 7) "yuriy_track4")
 (director_wait_time 1)
-(say "andrii" 	(get_dialog dialogs language 6))
-(say "andrii" 	(get_dialog dialogs language 7))
-(say "andrii" 	(get_dialog dialogs language 8))
-(say "yuriy" 	(get_dialog dialogs language 9))
+(talk "andrii" 	(get_dialog dialogs language 8)  "andrii_track5")
+(talk "andrii" 	(get_dialog dialogs language 9)  "andrii_track6")
+(talk "andrii" 	(get_dialog dialogs language 10) "andrii_track7")
+(talk "yuriy" 	(get_dialog dialogs language 11) "yuriy_track5")
+(animation "andrii" "back")
+(talk "andrii"  (get_dialog dialogs language 12) "andrii_track8")
+(talk "yuriy"   (get_dialog dialogs language 13) "yuriy_track6")
 (director_wait_time 1)
 (animation "andrii" "back_code")
 (stagecrafter_set_item "computer_C" "coding_C")
-(say "andrii" 	(get_dialog dialogs language 10))
+(talk "andrii"  (get_dialog dialogs language 14) "andrii_track9")
 (director_wait_time 1)
 (stagecrafter_set_item "shutdown" "trigger")
-(director_wait_time 2)
-(say "andrii" 	(get_dialog dialogs language 11))
+(talk "andrii" 	(get_dialog dialogs language 15) "andrii_track10")
 (animation "yuriy" 	"back_code")
 (stagecrafter_set_item "computer_A" "coding_A")
-(say "yuriy" 	(get_dialog dialogs language 12))
-(say "yuriy" 	(get_dialog dialogs language 13))
-(say "andrii" 	(get_dialog dialogs language 14))
+(talk "yuriy" 	(get_dialog dialogs language 16) "yuriy_track7")
+(talk "andrii" 	(get_dialog dialogs language 17) "andrii_track11")
 (effecteer_add "smoke1" "smoke_spot_particle" 2250 700 4 0 nil nil)
 (director_wait_time 1)
 (animation "andrii" "back")
@@ -269,21 +298,20 @@
 (stagecrafter_set_item "computer_B" "warning_B")
 (stagecrafter_set_item "computer_C" "still_C")
 (director_wait_time 1)
-(say "yuriy"  	(get_dialog dialogs language 15))
+(talk "yuriy"  	(get_dialog dialogs language 18) "yuriy_track8")
 (animation "yuriy" 	"still_right")
-(say "yuriy"  	(get_dialog dialogs language 16))
+(talk "yuriy"  	(get_dialog dialogs language 19) "yuriy_track9")
 (animation "andrii"	"still_left")
-(say "andrii"  	(get_dialog dialogs language 17))
-(animation "yuriy"	"still_right")
-(say "yuriy"  	(get_dialog dialogs language 18))
+(talk "andrii"  (get_dialog dialogs language 20) "andrii_track12")
+(talk "yuriy"  	(get_dialog dialogs language 21) "yuriy_track10")
 (director_wait_time 1)
 (animation "andrii"	"back_code")
 (stagecrafter_set_item "computer_C" "coding_C")
-(say "andrii" 	(get_dialog dialogs language 19))
+(talk "andrii" 	(get_dialog dialogs language 22) "andrii_track13")
 (animation "yuriy" 	"back")
-(say "yuriy"  	(get_dialog dialogs language 20))
-(say "yuriy"  	(get_dialog dialogs language 21))
-(say "yuriy"  	(get_dialog dialogs language 22))
+(talk "yuriy"  	(get_dialog dialogs language 23) "yuriy_track11A")
+(talk "yuriy"  	(get_dialog dialogs language 24) "yuriy_track11B")
+(talk "yuriy"  	(get_dialog dialogs language 25) "yuriy_track11C")
 (stagecrafter_set_item "computer_B" "error_B")
 (stagecrafter_set_item "lhc_segment_B" "move_silence")
 (stagecrafter_set_item "lhc_segment_D" "move_silence")
@@ -296,22 +324,22 @@
 (animation "andrii" "back")
 (stagecrafter_set_item "computer_C" "still_C")
 (director_wait_time 2)
-(say "yuriy" 	(get_dialog dialogs language 23))
+(talk "yuriy" 	(get_dialog dialogs language 26) "yuriy_track12")
 (director_wait_time 2)
 (puppeteer_move "andrii" 2240)
-(say_and_go "andrii" 	(get_dialog dialogs language 24))
-(say_and_go "andrii" 	(get_dialog dialogs language 25))
-(say_and_go "andrii" 	(get_dialog dialogs language 26))
+(talk_and_go "andrii" (get_dialog dialogs language 27) "andrii_track14A")
+(talk_and_go "andrii" (get_dialog dialogs language 28) "andrii_track14B")
+(talk_and_go "andrii" (get_dialog dialogs language 29) "andrii_track14C")
 (director_wait_movement "andrii")
 (animation "andrii" "back_code")
 (stagecrafter_set_item "computer_B" "error_coding_B")
-(say "andrii" 	(get_dialog dialogs language 27))
+(talk "andrii" 	(get_dialog dialogs language 30) "andrii_track15")
 (animation "yuriy" 	"still_right")
-(say "yuriy"  	(get_dialog dialogs language 28))
-(say "yuriy"  	(get_dialog dialogs language 29))
-(say "yuriy"  	(get_dialog dialogs language 30))
-(say "andrii" 	(get_dialog dialogs language 31))
-(say "andrii" 	(get_dialog dialogs language 32))
+(talk "yuriy"  	(get_dialog dialogs language 31) "yuriy_track13A")
+(talk "yuriy"  	(get_dialog dialogs language 32) "yuriy_track13B")
+(talk "yuriy"  	(get_dialog dialogs language 33) "yuriy_track13C")
+(talk "andrii" 	(get_dialog dialogs language 34) "andrii_track16A")
+(talk "andrii" 	(get_dialog dialogs language 35) "andrii_track16B")
 (stagecrafter_set_item "lhc_segment_A" "still")
 (stagecrafter_set_item "lhc_segment_B" "still")
 (stagecrafter_set_item "lhc_segment_C" "still")
@@ -319,50 +347,51 @@
 (stagecrafter_set_item "lhc_segment_E" "still")
 (effecteer_delete 	"redalarm1")
 (effecteer_stop 	"fire1")
+(effecteer_stop 	"smoke1")
 (director_wait_time 1)
 (animation "yuriy"  "back")
 (animation "andrii" "back")
 (stagecrafter_set_item "computer_B" "still_B")
 (director_wait_time 1)
-(say "yuriy"	(get_dialog dialogs language 33))
-(say "yuriy"  	(get_dialog dialogs language 34))
-(effecteer_stop 	"smoke1")
-(say "andrii" 	(get_dialog dialogs language 35))
+(talk "yuriy"	(get_dialog dialogs language 36) "yuriy_track14")
+(talk "andrii" 	(get_dialog dialogs language 37) "andrii_track17")
 (animation "yuriy" 	"still_right")
-(say "yuriy" 	(get_dialog dialogs language 36))
+(talk "yuriy" 	(get_dialog dialogs language 38) "yuriy_track15")
 (animation "andrii"	"still_left")
-(say "andrii"	(get_dialog dialogs language 37))
-(say "yuriy"	(get_dialog dialogs language 38))
+(talk "andrii"	(get_dialog dialogs language 39) "andrii_track18")
+(talk "yuriy"	(get_dialog dialogs language 40) "yuriy_track16")
 (animation "andrii"	"back")
-(say "andrii"	(get_dialog dialogs language 39))
-(say "andrii"	(get_dialog dialogs language 40))
-(say "andrii"	(get_dialog dialogs language 41))
+(talk "andrii"	(get_dialog dialogs language 41) "andrii_track19A")
+(talk "andrii"	(get_dialog dialogs language 42) "andrii_track19B")
+(talk "andrii"	(get_dialog dialogs language 43) "andrii_track19C")
+(talk "andrii"	(get_dialog dialogs language 44) "andrii_track19D")
 (effecteer_add "flash1" "thunder_flash"  0 0 4 0 t nil)
 (stagecrafter_set_item "thunder_strike" "strike")
 (director_wait_time 3)
 (animation "yuriy"	"still_left")
-(say "yuriy"	(get_dialog dialogs language 42))
+(director_wait_time 2)
+(talk "yuriy"	(get_dialog dialogs language 45) "yuriy_track17")
 (director_wait_time 1)
 (animation "yuriy" "back")
-(say "yuriy"	(get_dialog dialogs language 43))
+(talk "yuriy"	(get_dialog dialogs language 46) "yuriy_track18")
 (animation "yuriy" "still_right")
-(say "yuriy"	(get_dialog dialogs language 44))
+(talk "yuriy"	(get_dialog dialogs language 47) "yuriy_track19")
 (animation "andrii" "still_left")
-(say "andrii"	(get_dialog dialogs language 45))
-(say "yuriy"	(get_dialog dialogs language 46))
-(say "andrii"	(get_dialog dialogs language 47))
-(say "yuriy"	(get_dialog dialogs language 48))
-(say "andrii"	(get_dialog dialogs language 49))
+(talk "andrii"	(get_dialog dialogs language 48) "andrii_track20")
+(talk "yuriy"	(get_dialog dialogs language 49) "yuriy_track20")
+(talk "andrii"	(get_dialog dialogs language 50) "andrii_track21")
+(talk "yuriy"	(get_dialog dialogs language 51) "yuriy_track21")
+(talk "andrii"	(get_dialog dialogs language 52) "andrii_track22")
 (animation "yuriy" "back")
-(say "yuriy"	(get_dialog dialogs language 50))
+(talk "yuriy"	(get_dialog dialogs language 53) "yuriy_track22")
 (director_wait_time 1)
 (animation "andrii" "back")
 (director_wait_time 2)
 (effecteer_play 	"dream" "dream_a_little_dream_of_me" 4000 4000)
 (animation "yuriy" 	"still_right")
-(say "yuriy"	(get_dialog dialogs language 51))
+(talk "yuriy"	(get_dialog dialogs language 54) "yuriy_track23")
 (animation "andrii" "still_left")
-(say "andrii"	(get_dialog dialogs language 52))
+(talk "andrii"	(get_dialog dialogs language 55) "andrii_track23")
 (director_wait_time 1)
 (effecteer_add  "black1" "black_hole_animation" 0 -1100 28 0 t nil)
 (director_wait_time 5)
