55e4013150b067277b23ccae07eab6797d0557d2
nardinan <andrea@nardinan.it>
2017-10-20 18:47:27 +0200

    Bug in change of scenario fixed
    A bug in the change of the scenario script has been fixed. By pressing twice the space bar when over a change-scenario arrow the script was enqueued twice and the character was moved to the default position of the next scenario. The bug prevents the use to press more than once the space button.

--- data/scripts/movement_scripts/change_scenario.lisp
+++ data/scripts/movement_scripts/change_scenario.lisp
@@ -1,18 +1,21 @@
-; Pomodoro
-; Module: change_scenario
-; Author: nardinan
-; Date  : 15 Dec 2016
+;Pomodoro - I have no money and I must eat an ice cream
+;Module: change_scenario
+;Author: nardinan
+;Date  : 15 Dec 2016
+;Disables the control to prevent the same script to start multiple time (fixes the double-click bug)
+(puppeteer_disable_control "andrea")
 
+;Retrieves from the collector the destination that we have to reach
 (define destination_scenario (collector_get "destination_scenario"))
 
-; Clear out the scene with a movement outside
+;Cleans out the scene with a movement outside
 (director_camera_move 2000.0 8000.0)
-(director_wait_time 2)
+(director_wait_time 3)
 
-; Remove all the character (intro-script will take care of them)
-(puppeteer_hide) 
+;Removes all the character (intro-script will take care of them)
+(puppeteer_hide)
 (stagecrafter_hide)
 
-; Show the scenario
+;Show the scenario
 (director_camera_set 1000.0 -1000.0)
 (stagecrafter_show destination_scenario)
