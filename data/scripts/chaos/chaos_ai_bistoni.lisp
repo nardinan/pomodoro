c4cc5d2ef69489ac9d9ad33cabea1e7ae1a690e2
nardinan <andrea@nardinan.it>
2017-11-26 20:31:41 +0100

    Bistoni dialog implemented for chaos
    Dialog with Bistoni has been integrated for the chapter 'chaos'

---
+++ data/scripts/chaos/chaos_ai_bistoni.lisp
@@ -0,0 +1,133 @@
+;Pomodoro - I have no money and I must eat an ice cream
+;this script has been generated automatically and then has been polished manually
+;(sounds professional, uh? Well, it is not)
+;Code name  : <none>
+;Location   : UnipgA
+;Author     : Nardinan
+;Description: Bistoni tells you where to ask for the password
+
+;High level functions
+;@brief: say <character> <message>
+;@description: character <character> says <message> and the system waits for the bubble to disappear
+(define say
+  (lambda (c m)
+    (begin
+      (puppeteer_say c m (+ 1 (* (length m) 0.05)))
+      (director_wait_message c)
+      )
+    )
+  )
+
+;@brief: talk <character> <message> <track>
+;@description: similar to 'say', but is it possible to associate an audio track to the dialog
+(define talk
+  (lambda (c m track)
+    (begin
+      (puppeteer_say c m track)
+      (director_wait_message c)
+      )
+    )
+  )
+
+;@brief: say_and_go <character> <message>
+;@description: similar to 'say', but the control is returned immediately to the program before the end of the message
+(define say_and_go
+  (lambda (c m)
+    (begin
+      (puppeteer_say c m (+ 1 (* (length m) 0.05)))
+      )
+    )
+  )
+
+;@brief: talk_and_go <character> <message> <track>
+;@description: similar to 'talk', but the control is returned immediately to the program before the end of the audio track
+(define talk_and_go
+  (lambda (c m track)
+    (begin
+      (puppeteer_say c m track)
+      )
+    )
+  )
+
+;@brief: main_control <character>
+;@description: gives the main control of the game to the character <character>
+(define main_control
+  (lambda (c)
+    (begin
+      (director_camera_follow c -100.0 1.0)
+      (puppeteer_enable_control c)
+      )
+    )
+  )
+
+;@brief: get_dialog <dialog list> <language> <number>
+;@description: returns the message number <number> stored in the <dialog list> list in the <language> language
+(define get_dialog
+  (lambda (d lang n)
+    (begin
+      (if (= n 0)
+        (if (= lang 0)
+          (car (car d))
+          (cdr (car d))
+          )
+        (get_dialog (cdr d) lang (- n 1))
+        )
+      )
+    )
+  )
+
+;Environment configuration (music, effect, whatever)
+(puppeteer_disable_control)
+
+;Parameters configuration
+(define dialog_intro_done         (collector_get "bistoni_intro_chaos_dialog"))
+(define dialog_done_done          (collector_get "bistoni_done_chaos_dialog"))
+(define request_authorization     (collector_get "request_authorization"))
+(define request_location          (collector_get "request_location"))
+(define request_password          (collector_get "request_password"))
+(define done_location             (collector_get "done_location"))
+
+;Action!
+(if (= done_location 1.0)
+  nil
+  (begin
+    (collector_set "chaos_bistoni_and_policeman_ongoing" 1.0)
+    (puppeteer_look "andrea" "bistoni")
+    (puppeteer_look "bistoni" "andrea")
+    (if (= request_password 1.0)
+      (begin
+        (if (= dialog_done_done 1.0)
+          (director_dialog "game_bistoni_done_after_0x0c")
+          (begin
+            (director_dialog "game_bistoni_done_first_0x0c")
+
+            ;And never again
+            (collector_set "bistoni_done_chaos_dialog" 1.0))
+          )
+        (director_wait_dialog)
+        )
+      (begin
+        (if (= dialog_intro_done 1.0)
+          (director_dialog "game_bistoni_intro_after_0x0c")
+          (begin
+            (director_dialog "game_bistoni_intro_first_0x0c")
+
+            ;And never again
+            (collector_set "bistoni_intro_chaos_dialog" 1.0))
+          )
+
+        (director_wait_dialog)
+        (if (= request_authorization 1.0)
+          (if (= request_location 1.0)
+            (director_dialog "game_bistoni_requested_no_tool_0x0c")
+            (director_dialog "game_bistoni_activated_no_tool_0x0c")
+            )
+          (director_dialog "game_bistoni_no_request_0x0c")
+          )
+        (director_wait_dialog))
+      )
+    (collector_set "chaos_bistoni_and_policeman_ongoing" 0.0))
+  )
+
+;Return the control
+(main_control "andrea")
--- data/scripts/chaos/chaos_update_november_short.lisp
+++ data/scripts/chaos/chaos_update_november_short.lisp
@@ -27,6 +27,21 @@
                   (cons ;5 bistoni
                     "We cannot let them get away with this . . ."
                     "Non possono passarla liscia cosi' . . .")
+                  (cons ;6 bistoni
+                    "#include <bistoni.h>"
+                    "#include <bistoni.h>")
+                  (cons ;7 policeman
+                    "#include <policeman.h>"
+                    "#include <policeman.h>")
+                  (cons ;8 bistoni
+                    "int main(void)"
+                    "int main(void)")
+                  (cons ;9 policeman
+                    "main() wants argc, integer number of arguments, and a char* array."
+                    "main() prende argc, integer con numero di argomenti, e un array di puntatori a char.")
+                  (cons ;10 bistoni
+                    "OK let's start from scratch."
+                    "allora ricominciamo.")
                   nil
                   ))
 
@@ -113,29 +128,50 @@
 ;Parameters configuration
 (define chaos_bistoni_and_policeman_line      (collector_get "chaos_bistoni_and_policeman_line"))
 (define chaos_bistoni_and_policeman_ongoing   (collector_get "chaos_bistoni_and_policeman_ongoing"))
+(define request_password                      (collector_get "request_password"))
 
 ;Action!
 (if (= chaos_bistoni_and_policeman_ongoing 1.0)
   nil
-  (begin
-    (puppeteer_look "policeman" "bistoni")
-    (puppeteer_look "bistoni" "policeman")
-    (if (= chaos_bistoni_and_policeman_line 0.0)
-      (say "bistoni" (get_dialog dialogs language 1) "bistpolchaos_track1") ;preview: Please, officer. | looking at Policeman
-      (if (= chaos_bistoni_and_policeman_line 1.0)
-        (say "bistoni" (get_dialog dialogs language 2) "bistpolchaos_track2") ;preview: Are you listening me? | looking at Policeman
-        (if (= chaos_bistoni_and_policeman_line 2.0)
-          (say "bistoni" (get_dialog dialogs language 3) "bistpolchaos_track3") ;preview: I need your help! | looking at Policeman
-          (if (= chaos_bistoni_and_policeman_line 3.0)
-            (say "bistoni" (get_dialog dialogs language 4) "bistpolchaos_track4") ;preview: Police is never helping here! | looking at Policeman
-            (if (= chaos_bistoni_and_policeman_line 4.0)
-              (say "bistoni" (get_dialog dialogs language 5) "bistpolchaos_track5") ;preview: We cannot let them get away ... | looking at Policeman
-              nil
+  (if (= request_password 1.0)
+    (begin
+      (puppeteer_look "policeman" "bistoni")
+      (puppeteer_look "bistoni" "policeman")
+      (if (= chaos_bistoni_and_policeman_line 0.0)
+        (say "bistoni" (get_dialog dialogs language 6) "bistpolchaos_track6") ;preview: #include <bistoni.h> | looking at Policeman
+        (if (= chaos_bistoni_and_policeman_line 1.0)
+          (say "policeman" (get_dialog dialogs language 7) "bistpolchaos_track7") ;preview: #include <policeman.h> | looking at Bistoni
+          (if (= chaos_bistoni_and_policeman_line 2.0)
+            (say "bistoni" (get_dialog dialogs language 8) "bistpolchaos_track8") ;preview: int main(void) | looking at Policeman
+            (if (= chaos_bistoni_and_policeman_line 3.0)
+              (say "policeman" (get_dialog dialogs language 9) "bistpolchaos_track9") ;preview: main() wants argc, integer n... | looking at Bistoni
+              (if (= chaos_bistoni_and_policeman_line 4.0)
+                (say "bistoni" (get_dialog dialogs language 10) "bistpolchaos_track10") ;preview: OK let's start from scratch. | looking at Policeman
+                nil
+                )
               )
             )
           )
-        )
-      )
+        ))
+    (begin
+      (puppeteer_look "policeman" "bistoni")
+      (puppeteer_look "bistoni" "policeman")
+      (if (= chaos_bistoni_and_policeman_line 0.0)
+        (say "bistoni" (get_dialog dialogs language 1) "bistpolchaos_track1") ;preview: Please, officer. | looking at Policeman
+        (if (= chaos_bistoni_and_policeman_line 1.0)
+          (say "bistoni" (get_dialog dialogs language 2) "bistpolchaos_track2") ;preview: Are you listening me? | looking at Policeman
+          (if (= chaos_bistoni_and_policeman_line 2.0)
+            (say "bistoni" (get_dialog dialogs language 3) "bistpolchaos_track3") ;preview: I need your help! | looking at Policeman
+            (if (= chaos_bistoni_and_policeman_line 3.0)
+              (say "bistoni" (get_dialog dialogs language 4) "bistpolchaos_track4") ;preview: Police is never helping here! | looking at Policeman
+              (if (= chaos_bistoni_and_policeman_line 4.0)
+                (say "bistoni" (get_dialog dialogs language 5) "bistpolchaos_track5") ;preview: We cannot let them get away ... | looking at Policeman
+                nil
+                )
+              )
+            )
+          )
+        ))
     )
   )
 
