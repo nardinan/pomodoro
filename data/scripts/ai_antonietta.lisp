94adbc3e7e41de02dc0b5ebd0bf5287d54d6ea3d
nardinan <andrea@nardinan.it>
2017-12-05 15:14:26 +0100

    Antonietta has been included for chaos
    Antonietta (that gives you a slice of the password) has been included in the chapter 'chaos'

--- data/scripts/ai_antonietta.lisp
+++ data/scripts/ai_antonietta.lisp
@@ -4,6 +4,9 @@
   (director_script "quiet_ai_antonietta")
   (if (compare current_chapter "virus")
     (director_script "virus_ai_antonietta")
-    nil
+    (if (compare current_chapter "chaos")
+      (director_script "chaos_ai_antonietta")
+      nil
+      )
     )
   )
